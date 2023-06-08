adecccf7c19fba291f48f59a00d05ff63260d9bb
belongsyb <73025548+belongsyb@users.noreply.github.com>
2023-06-08 20:58:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -17,9 +17,9 @@ const glados = async () => {
       headers,
     }).then((r) => r.json())
     return [
+      `剩余 ${Number(status.data.leftDays)} 天`,
       'Checkin OK',
       `${checkin.message}`,
-      `Left Days ${Number(status.data.leftDays)}`,
     ]
   } catch (error) {
     return [
